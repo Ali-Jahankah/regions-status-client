db7a7c186d3dc043cd0413e286e7155227fdfcde
Ali Jahankah <aral2@yahoo.com>
2024-08-17 18:38:55 +0100

    feat: add header options as a config file

---
+++ src/config/header.ts
@@ -0,0 +1,8 @@
+export const headerOptions = [
+  { text: 'eu-west' },
+  { text: 'eu-central' },
+  { text: 'sa-east' },
+  { text: 'ap-southeast' },
+  { text: 'us-west' },
+  { text: 'us-east' }
+];
