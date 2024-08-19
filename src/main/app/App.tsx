2ae50640134d053a9fafb0779711de11ba66fdda
Ali Jahankah <aral2@yahoo.com>
2024-08-20 00:11:43 +0100

    fix: edit App compont and Routes

--- src/main/app/App.tsx
+++ src/main/app/App.tsx
@@ -1,30 +1,47 @@
 import './App.css';
 
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { AllRegionsData, UpdateTimer } from '../../config/types';
+import React, { useEffect } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { regionsState, timerState } from '../../RecoilStateManagement/states';
 
+import CustomError from '../../components/errors/CustomError/CustomError';
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/header/Header';
 import HomePage from '../homePage/HomePage';
-import React from 'react';
+import NotFound from '../../components/errors/404/NotFound';
 import RegionPage from '../regionPage/RegionPage';
+import { useRecoilValue } from 'recoil';
+import { useWebSocketData } from '../../utils/customHooks/useWebSocketData';
 
-const App = (): React.ReactElement => {
-  //Adding React Router DOM to enable navigation between pages and update the URL accordingly
+//Adding React Router DOM to enable navigation between pages and update the URL accordingly
+
+const App: React.FC = (): React.ReactElement => {
+  useWebSocketData();
+  const navigate = useNavigate();
+  const timer: UpdateTimer | null = useRecoilValue(timerState);
+  const data: AllRegionsData | null = useRecoilValue(regionsState);
+
+  useEffect(() => {
+    // Navigate user to 500 error page if server stops or data does not exist
+    if (!data || !timer) {
+      navigate('/500');
+    }
+  }, [data, timer, navigate]);
   return (
-    <Router>
-      <main className="app-container">
-        <div className="glass-overlay">
-          <Header />
-
-          <Routes>
-            <Route path="/" element={<HomePage />}></Route>
-            <Route path="/regions/:region" element={<RegionPage />}></Route>
-          </Routes>
-
-          <Footer />
-        </div>
-      </main>
-    </Router>
+    <main className="app-container">
+      <div className="glass-overlay">
+        <Header allData={data} />
+        <Routes>
+          <Route path="/regions/:regionId" element={<RegionPage />}></Route>
+          <Route path="/500" element={<CustomError allData={data} />}></Route>
+          <Route path="/" element={<HomePage />}></Route>
+          {/* 404 page for all other routes */}
+          <Route path="*" element={<NotFound />}></Route>{' '}
+        </Routes>
+        <Footer />
+      </div>
+    </main>
   );
 };
 
