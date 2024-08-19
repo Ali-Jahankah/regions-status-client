ad4018445d57031a676594b4d851ae6e1e3a0ce1
Ali Jahankah <aral2@yahoo.com>
2024-08-20 00:08:35 +0100

    feat: create HomePage

--- src/main/homePage/HomePage.tsx
+++ src/main/homePage/HomePage.tsx
@@ -2,8 +2,19 @@ import './HomePage.css';
 
 import React from 'react';
 
-const HomePage = (): React.ReactElement => {
-  return <article className="homepage-container">HomePage</article>;
+const HomePage: React.FC = (): React.ReactElement => {
+  return (
+    <article className="homepage-container">
+      <div>
+        <p>Please click on any region to see live updates.</p>
+        <p>You can refresh the data every 60 seconds.</p>
+        <p>
+          If needed, you can later adjust the update interval and data retrieval
+          settings on either the client side or the server side.
+        </p>
+      </div>
+    </article>
+  );
 };
 
 export default HomePage;
