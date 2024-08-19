00b0ea20b7cb9a24e2027902011106d1f884b20e
Ali Jahankah <aral2@yahoo.com>
2024-08-20 00:14:14 +0100

    fix: add type

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import './Footer.css';
 
 import React from 'react';
 
-const Footer = (): React.ReactElement => {
+const Footer: React.FC = (): React.ReactElement => {
   return (
     <footer className="footer">
       Made by <a href="https://linktr.ee/uaral">Ali Jahankah</a> | 2024
